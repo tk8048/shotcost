9a507c125fb050ed1ad203c5b6c824d0c6218b07
tk8048 <55033360+tk8048@users.noreply.github.com>
2019-09-07 16:01:38 -0400

    Add files via upload

---
+++ shotcostcalc.m
@@ -0,0 +1,4 @@
+bottlePrice=input("Bottle Price? (USD)         ");
+bottleVolume=input("Bottle Volume? (mL)       ");
+shotcost=bottlePrice/(bottleVolume/44);
+disp("Price of one shot is:"),disp(shotcost)
